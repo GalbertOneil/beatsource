f7a9d69b1604d40fc0a9541e0035638344178852
galbertoneil <goneilp@gmail.com>
2019-05-10 14:34:08 -0400

    added pictures for readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import decode from 'jwt-decode';
 import Login from './components/Login'
